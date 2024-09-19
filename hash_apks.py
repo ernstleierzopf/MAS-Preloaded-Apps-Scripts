6693a336fd6ae3312644bc363d31878791562a51
Ernst Leierzopf <ernst.leierzopf@ins.jku.at>
2024-09-19 17:34:54 +0200

    fix apks path in hash_apks.py.

--- hash_apks.py
+++ hash_apks.py
@@ -2,9 +2,10 @@ import os
 import hashlib
 
 apk_hashes = "apk_hashes.csv"
+apks_path = "apks"
 
 with open(apk_hashes, "w") as hash_file:
-    for root, dirs, files in os.walk(walk_dir, topdown=True):
+    for root, dirs, files in os.walk(apks_path, topdown=True):
         for filename in files:
             file_path = os.path.join(root, filename)
             sha256 = hashlib.sha256()
